13797f260ef9b776a36fee17b71f2e90ef4aa290
Aaron Pavlick <apavlick@yext.com>
2022-04-16 14:07:14 -0400

    search context

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,32 @@ import { Header } from "./components/Header";
 import { DeliveryBanner } from "./components/DeliveryBanner";
 import { PageRouter } from "./PageRouter";
 import { routeConfig } from "./config/routeConfig";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+
+interface SearchContext {
+  active: boolean;
+  setActive: (active: boolean) => void;
+}
+
+const searchCtx: SearchContext = {
+  active: false,
+  setActive: (active: boolean) => {},
+};
+
+export const SearchCtx = createContext<SearchContext>(searchCtx);
 
 function App() {
+  const [active, setActive] = useState(false);
+  const value = useMemo(() => ({ active, setActive }), [active]);
+
   return (
-    <div className="font-primary">
-      <Header />
-      <DeliveryBanner />
-      <PageRouter routes={routeConfig} />
-    </div>
+    <SearchCtx.Provider value={value}>
+      <div className="font-primary">
+        <Header />
+        <DeliveryBanner />
+        <PageRouter routes={routeConfig} />
+      </div>
+    </SearchCtx.Provider>
   );
 }
 
