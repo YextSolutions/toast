ac0aa94fda92520d73484bf9315d65f31732ad98
Aaron Pavlick <apavlick@yext.com>
2022-04-17 15:16:14 -0400

    beverage results title

--- src/screens/BeverageResultsScreen.tsx
+++ src/screens/BeverageResultsScreen.tsx
@@ -1,4 +1,9 @@
-import { Matcher, SelectableFilter, useAnswersActions } from "@yext/answers-headless-react";
+import {
+  Matcher,
+  SelectableFilter,
+  useAnswersActions,
+  useAnswersState,
+} from "@yext/answers-headless-react";
 import { VerticalResults } from "@yext/answers-react-components";
 import { useContext, useEffect, useState } from "react";
 import { useLocation, useParams, useSearchParams } from "react-router-dom";
@@ -13,12 +18,16 @@ import classNames from "classnames";
 
 export const BeverageResultsScreen = () => {
   const [page, setPage] = useState("");
+  const [searchResultsTitle, setSearchResultsTitle] = useState<{ query?: boolean; title: string }>({
+    title: "",
+  });
 
   const urlParams = useParams();
   const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const answersActions = useAnswersActions();
+  const resultsCount = useAnswersState((state) => state.vertical.resultsCount);
 
   const { active, beverageResultImages } = useContext(SearchCtx);
 
@@ -26,34 +35,49 @@ export const BeverageResultsScreen = () => {
     const { alcoholType, category, subCategory } = extractBeverageInfoFromUrl(urlParams);
     const query = searchParams.get("query");
 
-    answersActions.setQuery(query ?? "");
+    if (query) {
+      answersActions.setQuery(query ?? "");
+      setSearchResultsTitle({ query: true, title: `Results for ${query}` });
+    }
 
     const selectedFilters: SelectableFilter[] = [];
 
-    alcoholType &&
+    if (alcoholType) {
       selectedFilters.push({
         selected: true,
         fieldId: "c_alcoholType",
         value: alcoholType,
         matcher: Matcher.Equals,
       });
+      !query &&
+        setSearchResultsTitle({
+          title: alcoholType.charAt(0).toUpperCase() + alcoholType.slice(1),
+        });
+    }
 
-    category &&
-      alcoholType !== "all" &&
+    if (category && category !== "all") {
       selectedFilters.push({
         selected: true,
         fieldId: "c_category",
         value: category.replaceAll("-", " "),
         matcher: Matcher.Equals,
       });
+      !query &&
+        setSearchResultsTitle({ title: category.charAt(0).toUpperCase() + category.slice(1) });
+    }
 
-    subCategory &&
+    if (subCategory) {
       selectedFilters.push({
         selected: true,
         fieldId: "c_subCategory",
         value: subCategory.replaceAll("-", " "),
         matcher: Matcher.Equals,
       });
+      !query &&
+        setSearchResultsTitle({
+          title: subCategory.charAt(0).toUpperCase() + subCategory.slice(1),
+        });
+    }
 
     answersActions.setStaticFilters(selectedFilters);
     answersActions.executeVerticalQuery();
@@ -70,8 +94,8 @@ export const BeverageResultsScreen = () => {
       <DeliveryBanner />
       {beverageResultImages[page] && (
         <div className="w-full flex justify-center">
-          <div className={classNames("w-full py-4 px-4 max-w-sm")}>
-            <img className="w-full" src={beverageResultImages[page]}></img>
+          <div className={" py-4 px-4 max-w-sm"}>
+            <img className="w-96 h-44" src={beverageResultImages[page]}></img>
           </div>
         </div>
       )}
@@ -80,6 +104,16 @@ export const BeverageResultsScreen = () => {
       ) : (
         <>
           <BeverageBreadcrumbs />
+          <div className="my-2 px-4">
+            <span
+              className={classNames("mr-1.5 text-3xl font-bold", {
+                "text-toast-dark-orange  ": !searchResultsTitle.query,
+              })}
+            >
+              {searchResultsTitle.title}
+            </span>
+            {`(${resultsCount} results)`}
+          </div>
           {/* TODO: add more promanent page title under image (either results for or highlighted category) */}
           <VerticalResults
             customCssClasses={{ results: "grid grid-cols-2 sm:grid-cols-3" }}
