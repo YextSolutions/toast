2c9302ee053a15be732e9b9f08fdc68e8ec2a3b7
Aaron Pavlick <apavlick@yext.com>
2022-08-09 17:00:56 -0400

    upgrade to React 18

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import ReactDOM from "react-dom/client";
 import { SearchHeadlessProvider, SandboxEndpoints } from "@yext/search-headless-react";
 import searchConfig from "./config/searchConfig";
 import { AppContextProvider } from "./providers/AppContextProvider";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SearchHeadlessProvider {...searchConfig} endpoints={SandboxEndpoints}>
       <AppContextProvider>
         <App />
       </AppContextProvider>
     </SearchHeadlessProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
